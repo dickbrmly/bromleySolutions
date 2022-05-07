6fdb5f2113f06887b6c90e19a051f1bbc37fd3fb
dickbrmly <dickbrmly@outlook.com>
2022-05-07 07:02:06 -0700

    merging toward  single server

--- server.js
+++ server.js
@@ -3,58 +3,60 @@
 var debug = require("debug");
 debug("server.js")("working");
 
-var express = require("express");
+const bodyParser = require('body-parser')
+const hostname = 'dick-bromley.com'
+const express = require('express')
+const https = require('https')
+const path = require('path')
+
 var app = express();
-app.use(express.urlencoded({extended:false}));
+app.use(express.urlencoded({ extended: false }));
 
 
 var mysql = require("mysql");
 var fs = require('fs');
 var http = require('http');
 
-
-
-
 let address = '127.0.0.1';
 var httpServer = http.createServer(app);
 
-   app.get('/', function (req, res) 
-   {
-      res.sendFile(__dirname + '/client/index.html');
-   });
-   app.post('/newEmail', function (req, res)
-   {
-      console.log('got here');
-   })
-   app.get('/*.css', function (req, res)
-   {
-      console.log(req.url);
-      res.sendFile(__dirname + req.url);
-   });
-   app.get('/*.js', function (req, res)
-   {
-      console.log(req.url);
-      res.sendFile(__dirname + req.url);
-   });
-   app.get('/*.html', function (req, res)
-   {
-      console.log(req.url);
-      res.sendFile(__dirname + '/client/' + req.url);
-   });
+app.get('/', function(req, res)
+{
+    res.sendFile(__dirname + '/client/index.html');
+});
+app.post('/newEmail', function(req, res)
+{
+    console.log('got here');
+})
+app.get('/*.css', function(req, res)
+{
+    console.log(req.url);
+    res.sendFile(__dirname + req.url);
+});
+app.get('/*.js', function(req, res)
+{
+    console.log(req.url);
+    res.sendFile(__dirname + req.url);
+});
+app.get('/*.html', function(req, res)
+{
+    console.log(req.url);
+    res.sendFile(__dirname + '/client/' + req.url);
+});
 
-   app.get('/*.png', function (req, res)
-   {
-      console.log(req.url);
-      res.sendFile(__dirname + req.url);
-   });
+app.get('/*.png', function(req, res)
+{
+    console.log(req.url);
+    res.sendFile(__dirname + req.url);
+});
 
-   app.get('/*.jpg', function (req, res)
-   {
-      console.log(req.url);
-      res.sendFile(__dirname + req.url);
-   });
+app.get('/*.jpg', function(req, res)
+{
+    console.log(req.url);
+    res.sendFile(__dirname + req.url);
+});
 
-   app.listen(8080, address, function ()
-   {
-      return console.log('Server Running');
-   });
+app.listen(8080, address, function()
+{
+    return console.log('Server Running');
+});
